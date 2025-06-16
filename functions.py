f5d004d733b52117c8317044f1314a7cd0a1d357
Dhiman K. Chakraborty <dhimankumarchakraborty@gmail.com>
2025-06-16 23:02:43 +0530

    Not working

--- functions.py
+++ functions.py
@@ -1,88 +1,115 @@
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
-import itertools
+from itertools import combinations
 
 
-def basis_generator(tot_sites, N_e): # Checked OK
+def basis_set_generator(tot_sites, N_particles): # Checked OK
+    basis = []
 
-  basis = []
-
-  for combination_indices in itertools.combinations(range(tot_sites), N_e):
-    state = [0] * tot_sites
-    for index in combination_indices:
-      state[index] = 1
-    basis.append(state)
-  
-  return np.array(basis, dtype=np.float64)
-
-
-def hamiltonian_generator_wo_int(basis_set, tot_sites, v, w):
-  d = len(basis_set)
-  H = np.zeros((d, d))
-
-  for i in range(d):
-    for j in range(d):
-      H[j][i] = np.dot(hamiltonian_on_ket_wo_int(basis_set[i], tot_sites, v, w), basis_set[j])
-
-  return H
+    for comb in combinations(range(tot_sites), N_particles):
+        state = [0] * tot_sites
+        for idx in comb:
+            state[idx] = 1
+        basis.append(state)
+    
+    return np.array(basis)
 
 
-def hamiltonian_on_ket_wo_int(ket, tot_sites, v, w):
-  res = np.zeros((tot_sites), dtype=np.float64)
-  for i in range(tot_sites):
-    if i % 2 == 0 and i - 1 > 0:
-      if ket[i - 1] == 1:
-        res[i] += w
-      if ket[i + 1] == 1:
-        res[i] += v
-    elif i % 2 == 1 and i + 1 < tot_sites:
-      if ket[i - 1] == 1:
-        res[i] += v
-      if ket[i + 1] == 1:
-        res[i] += w
-    elif i == 0 and ket[i + 1] == 1:
-      res[i] += v
-    elif i == tot_sites - 1 and ket[i - 1] == 1:
-      res[i] += v
-  return res
+def state_idx_mapping(basis_set): # Checked OK
+    return {tuple(state) : i for i, state in enumerate(basis_set)}
 
 
-def hamiltonian_generator_w_int(basis_set, tot_sites, v, w, V1, V2):
-  d = len(basis_set)
-  H = np.zeros((d, d))
+def creation_operator(state, i): # Checked OK
+    if state[i] == 1:
+        return 0, None
+    
+    res_state = state.copy()
+    res_state[i] = 1
 
-  for i in range(d):
-    for j in range(d):
-      H[j][i] = np.dot(hamiltonian_on_ket_w_int_diff_V(basis_set[i], tot_sites, v, w), basis_set[j], V1, V2)
+    sign = 1
+    for j in range(i):
+        if res_state[j] == 1:
+            sign *= -1
+    
+    return sign, res_state
 
-  return H
+
+def annihilation_operator(state, i): # Checked OK
+    if state[i] == 0:
+        return 0, None
+    
+    res_state = state.copy()
+    res_state[i] = 0
+
+    sign = 1
+    for j in range(i):
+        if res_state[j] == 1:
+            sign *= -1
+    
+    return sign, res_state
 
 
-def hamiltonian_on_ket_w_int_diff_V(ket, tot_sites, v, w, V1, V2):
-  res = np.zeros((tot_sites), dtype=np.float64)
-  for i in range(tot_sites):
-    if i % 2 == 0 and i - 1 > 0:
-      if ket[i - 1] == 1:
-        res[i] += w
-      if ket[i + 1] == 1:
-        res[i] += v
-        if ket[i] == 1:
-          res[i] += V1
-    elif i % 2 == 1 and i + 1 < tot_sites:
-      if ket[i - 1] == 1:
-        res[i] += v
-      if ket[i + 1] == 1:
-        res[i] += w
-        if ket[i] == 1:
-          res[i] += V2
-    elif i == 0 and ket[i + 1] == 1:
-      res[i] += v
-      if ket[i] == 1:
-          res[i] += V1
-    elif i == tot_sites - 1 and ket[i - 1] == 1:
-      res[i] += v
-  return res
+def hopping_operator(state, initial_position, target_position): # Checked OK
+    sign_1, state_1 = annihilation_operator(state, initial_position)
+
+    # if sign_1 == 0:
+    #     return 0, None
+
+    sign_2, state_2 = creation_operator(state_1, target_position)
+
+    return sign_1 * sign_2, state_2
+
+
+def hamiltonian_matrix_generator(basis_set, tot_sites, v, w, V1, V2): # Checked OK
+    dim = len(basis_set)
+    hamiltonian = np.zeros((dim, dim), dtype=np.float64)
+
+    state_idx_dict = state_idx_mapping(basis_set)
+
+    for i, state in enumerate(basis_set):
+        for j in range(tot_sites):
+            if state[j] == 0:
+                continue
+
+            if j + 1 < tot_sites:
+                if state[j + 1] == 0:
+                    initial_position = j
+                    target_position = j + 1
+                    # print(initial_position, target_position)
+                    sign, res_state = hopping_operator(state, initial_position, target_position)
+                    res_state_idx = state_idx_dict[tuple(res_state)]
+
+                    if j % 2 == 0:
+                        hamiltonian[res_state_idx, i] += v * sign
+                    else:
+                        hamiltonian[res_state_idx, i] += w * sign
+                
+                elif state[j + 1] == 1:
+                    if j % 2 == 0:
+                        hamiltonian[i, i] += V1
+                    else:
+                        hamiltonian[i, i] += V2
+
+            if j - 1 >= 0:
+                if state[j - 1] == 0:
+                    initial_position = j
+                    target_position = j - 1
+                    sign, res_state = hopping_operator(state, initial_position, target_position)
+                    res_state_idx = state_idx_dict[tuple(res_state)]
+
+                    if j % 2 == 0:
+                        hamiltonian[res_state_idx, i] += w * sign
+                    else:
+                        hamiltonian[res_state_idx, i] += v * sign
+                
+                elif state[j - 1] == 1:
+                    if j % 2 == 0:
+                        hamiltonian[i, i] += V2
+                    else:
+                        hamiltonian[i, i] += V1
+    
+    return hamiltonian
 
 
 def normalize(vec):
